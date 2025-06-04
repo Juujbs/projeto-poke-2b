7aa83654b3aa369ae9682f93958480e2d8c03485
Juujbs <rm94612@estudante.fieb.edu.br>
2025-06-04 14:47:52 +0000

    PokeFlex e PokeGrid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,15 @@
 import poke from "./assets/pokeapi_256.png"
 import eevee from './assets/eevee.jpg'
 import './Home.css'
- 
+import PokeFlex from "./Pokeflex"
+import PokeGrid from "./PokeGrid"
+
 const Home = ()=> {
     return (
         <div className="pokeflex">
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />
-            <img src={eevee} />
-            <img src={poke} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            <img src={eevee} />
-            <img src={poke} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            <img src={eevee} />
-            <img src={poke} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            <img src={eevee} />
-             <img src={poke} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            <img src={eevee} />
-             <img src={poke} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            <img src={eevee} />
-             <img src={poke} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            <img src={eevee} />
+            <PokeFlex />
+            <PokeGrid />
+
         </div>
     )
 }
